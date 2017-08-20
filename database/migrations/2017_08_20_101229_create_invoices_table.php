42e4e38caf88aa8b380fc37c5e2adbe890c79fa6
Zane J. Chua <zane.jocelyn@gmail.com>
2017-08-20 18:49:51 +0800

    Initial Commit with Migrations

---
+++ database/migrations/2017_08_20_101229_create_invoices_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('invoiceid');
+            $table->dateTime('date');
+            $table->dateTime('duedate');
+            $table->integer('netdays');
+            $table->text('items');
+            $table->integer('client_id')->unsigned();
+            $table->foreign('client_id')
+                ->references('id')->on('clients')
+                ->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+}
---
+++ readme.md
@@ -0,0 +1,35 @@
+
+# Invoice Plz
+
+## Setup and Deployment
+```bash
+composer install
+npm install
+npm run dev
+cp .env.example .env
+php artisan key:generate
+php artisan migrate
+php artisan db:seed
+php artisan serve --host 0.0.0.0
+```
+
+## About Invoice Plz
+
+An Invoice System developed for anyone who needs to generate out an invoice and manage clients
+
+
+## License
+
+Copyright 2018 Poowf Labs LLP
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
