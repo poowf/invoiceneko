9001da148fefc25afb30d80d4cc50b6a80aa279d
Zane J. Chua <zane.jocelyn@gmail.com>
2017-09-10 22:36:42 +0800

    Laravel 5.5 Upgrade

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,12 +1,26 @@
 
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-window._ = require('lodash');
+/*require('./bootstrap');
+
+window.Vue = require('vue');*/
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+/*Vue.component('example', require('./components/Example.vue'));
+
+const app = new Vue({
+    el: '#app'
+});
+*/
 
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
@@ -18,36 +32,3 @@ window.$ = window.jQuery = require('jquery');
 //window.Laravel = { csrfToken: $('meta[name=csrf-token]').attr("content") };
 
 require('materialize-css');
-
-/**
- * Vue is a modern JavaScript library for building interactive web interfaces
- * using reactive data binding and reusable components. Vue's API is clean
- * and simple, leaving you to focus on building your next great project.
- */
-
-window.Vue = require('vue');
-
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
-
-window.axios = require('axios');
-
-window.axios.defaults.headers.common = {
-    'X-CSRF-TOKEN': window.Laravel.csrfToken,
-    'X-Requested-With': 'XMLHttpRequest'
-};
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-// Vue.component('example', require('./components/Example.vue'));
-//
-// const app = new Vue({
-//     el: '#app'
-// });
