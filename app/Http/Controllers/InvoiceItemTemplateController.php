dff65b50c1dd42a527e627d1c392c91aa8e1d5c2
Zane J. Chua <zane.jocelyn@gmail.com>
2018-10-18 10:00:06 +0000

    Initial Implementation of InvoiceItemTemplates, added Cloning of Invoices, bug fixes

---
+++ app/Http/Controllers/InvoiceItemTemplateController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\InvoiceItemTemplate;
+use Illuminate\Http\Request;
+
+class InvoiceItemTemplateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\InvoiceItemTemplate  $invoiceItemTemplate
+     * @return \Illuminate\Http\Response
+     */
+    public function show(InvoiceItemTemplate $invoiceItemTemplate)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\InvoiceItemTemplate  $invoiceItemTemplate
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(InvoiceItemTemplate $invoiceItemTemplate)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\InvoiceItemTemplate  $invoiceItemTemplate
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, InvoiceItemTemplate $invoiceItemTemplate)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\InvoiceItemTemplate  $invoiceItemTemplate
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(InvoiceItemTemplate $invoiceItemTemplate)
+    {
+        //
+    }
+}
