2f8444914be86e6a89a621e4ad9c3fba23c52fd5
Zane J. Chua <zane.jocelyn@gmail.com>
2018-10-18 23:48:40 +0800

    Refactor ItemTemplate to be Generic for both Quote and Invoices

--- app/Models/ItemTemplate.php
+++ app/Models/ItemTemplate.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Iatstuti\Database\Support\CascadeSoftDeletes;
 
-class InvoiceItemTemplate extends Model
+class ItemTemplate extends Model
 {
     use SoftDeletes, CascadeSoftDeletes;
 
