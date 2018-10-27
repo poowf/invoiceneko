015b351b5c325f9ed233d3f29be76be159dc9bb0
Zane J. Chua <zane.jocelyn@gmail.com>
2018-10-28 01:46:19 +0800

    Invoice and Quote Mobile Optimisations

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -57,6 +57,16 @@
             });
         });
 
+        $('a[href^="#"]').on('click', function(event) {
+            let target = $(this.getAttribute('href'));
+            if( target.length ) {
+                event.preventDefault();
+                $('html, body').stop().animate({
+                    scrollTop: target.offset().top
+                }, 500, 'linear');
+            }
+        });
+
         @if(session()->has('flash_notification'))
             @foreach (session('flash_notification', collect())->toArray() as $message)
                 @if ($message['overlay'])
