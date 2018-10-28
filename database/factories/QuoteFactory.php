f67740a3b63c61b608b5280e2b43037136d8f7d7
Zane J. Chua <zane.jocelyn@gmail.com>
2018-10-28 13:34:33 +0000

    Initial implementation for Tests, Configuring of Data Factories

---
+++ database/factories/QuoteFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(Model::class, function (Faker $faker) {
+    return [
+        'nice_quote_id' => $faker->slug,
+        'date' => $faker->dateTime,
+        'duedate' => $faker->dateTime,
+        'netdays' => $faker->numberBetween($min = 1, $max = 60),
+        'total' => $faker->randomFloat($nbMaxDecimals = NULL, $min = 0, $max = NULL),
+        'share_token' => $faker->uuid,
+        'status' => $faker->numberBetween($min = 1, $max = 7),
+        'archived' => $faker->boolean,
+    ];
+});
