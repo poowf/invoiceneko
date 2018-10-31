bf657aaf25b95c3ecd78f2f2b274d42960903f77
Zane J. Chua <zane.jocelyn@gmail.com>
2018-11-01 00:07:59 +0800

    Should be the final implementation for multiusers

---
+++ database/migrations/2018_10_31_221557_create_company_user_requests_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompanyUserRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company_user_requests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('full_name');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->string('token');
+            $table->integer('status');
+            $table->integer('company_id')->unsigned();
+            $table->foreign('company_id')
+                ->references('id')->on('companies')
+                ->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('company_user_requests');
+    }
+}
