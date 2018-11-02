cc52cc67f4012242e04ffd0d7e6da0c14b826e8c
Zane J. Chua <zane.jocelyn@gmail.com>
2018-11-02 11:01:01 +0000

    New Model for Invoice Events and UI for creating recurring invoices

---
+++ app/Models/InvoiceEvent.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Log;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Iatstuti\Database\Support\CascadeSoftDeletes;
+
+class InvoiceEvent extends Model
+{
+    use SoftDeletes, CascadeSoftDeletes;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'invoice_events';
+
+    public function invoice()
+    {
+        return $this->belongsTo('App\Models\Invoice', 'invoice_id');
+    }
+}
