838d9e91c73eb1ac42f63fede3ea40d226e487aa
Zane J. Chua <zane.jocelyn@gmail.com>
2018-11-05 01:24:44 +0800

    Update to factories and fix some errors in migrations

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -8,6 +8,7 @@ $factory->define(\App\Models\Company::class, function (Faker $faker) {
         'invoice_index' => $faker->randomDigit,
         'quote_index' => $faker->randomDigit,
         'slug' => $faker->slug,
+        'domain_name' => $faker->domainName,
         'crn' => $faker->ean8,
         'phone' => '+65' . $faker->randomNumber(8),
         'email' => $faker->unique()->companyEmail,
