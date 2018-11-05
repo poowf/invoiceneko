8040b200d23745934609c2b3c10fe4b9e49c03b1
Zane J. Chua <zane.jocelyn@gmail.com>
2018-11-05 22:56:51 +0800

    Changes to Invoice, Quote and Payment pages for Creation, Update TestCases

--- database/factories/ItemTemplateFactory.php
+++ database/factories/ItemTemplateFactory.php
@@ -4,9 +4,9 @@ use Faker\Generator as Faker;
 
 $factory->define(\App\Models\ItemTemplate::class, function (Faker $faker) {
     return [
-        'name' => $faker->realText($maxNbChars = 20, $indexSize = 1),
+        'name' => $faker->text($maxNbChars = 20),
         'quantity' => $faker->numberBetween($min = 1, $max = 1000),
-        'price' => $faker->randomFloat($nbMaxDecimals = NULL, $min = 0, $max = NULL),
+        'price' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = NULL),
         'description' => $faker->randomHtml(2,3),
         'company_id' => function () {
             return factory(\App\Models\Company::class)->create()->id;
