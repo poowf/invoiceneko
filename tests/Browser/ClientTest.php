12935233068b89b17d159b9e5c1160859337c7a1
Zane J. Chua <zane.jocelyn@gmail.com>
2018-11-06 00:47:26 +0800

    Add More Tests, fix more factories and updated Travis CI Configuration

--- tests/Browser/ClientTest.php
+++ tests/Browser/ClientTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Browser;
 
+use App\Models\Company;
 use Faker\Factory as Faker;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
@@ -17,11 +18,36 @@ class ClientTest extends DuskTestCase
      * @return void
      * @throws \Throwable
      */
-    public function testExample()
+    public function test_create_a_client()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                ->assertPresent('#signin-btn');
+        $company = factory(Company::class)->create();
+        //Need to assign the company_id to the user
+        $company->owner->company_id = $company->id;
+        $company->owner->save();
+
+        $faker = Faker::create();
+        $salutation = ["mr", "mrs", "mdm", "miss"];
+
+        $this->browse(function (Browser $browser) use ($faker, $company, $salutation) {
+            $browser->visit('/signin')
+                ->type('username', $company->owner->email)
+                ->type('password', 'secret')
+                ->press('SIGN IN')
+                ->assertPathIs('/dashboard')
+                ->visit('/clients')
+                ->click("a[href='{$this->baseUrl()}/client/create']")
+                ->assertPathIs('/client/create')
+                ->type('companyname', $faker->company)
+                ->type('block', $faker->buildingNumber)
+                ->type('street', $faker->streetName)
+                ->type('contactfirstname', $faker->firstName)
+                ->type('contactemail', $faker->unique()->companyEmail)
+                ->type('contactphone', '+658' . $faker->numberBetween($min = 1, $max = 8) . $faker->randomNumber(6));
+            $browser
+                ->script('jQuery("#contactsalutation").selectize()[0].selectize.setValue("mr");');
+            $browser
+                ->press('CREATE')
+                ->assertPresent('#client-container');
         });
     }
 }
