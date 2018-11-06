455c9994435768ee5ac9a75a94752948d9be241a
Zane J. Chua <zane.jocelyn@gmail.com>
2018-11-06 22:58:39 +0800

    Added Company Test

--- tests/Browser/CompanyTest.php
+++ tests/Browser/CompanyTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Browser;
 
+use App\Models\Company;
 use Faker\Factory as Faker;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
@@ -17,11 +18,30 @@ class CompanyTest extends DuskTestCase
      * @return void
      * @throws \Throwable
      */
-    public function testExample()
+    public function test_updating_a_company()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                ->assertPresent('#signin-btn');
+        $company = factory(Company::class)->create();
+        //Need to assign the company_id to the user
+        $company->owner->company_id = $company->id;
+        $company->owner->save();
+
+        $faker = Faker::create();
+
+        $this->browse(function (Browser $browser) use ($faker, $company) {
+            $browser->visit('/signin')
+                ->type('username', $company->owner->email)
+                ->type('password', 'secret')
+                ->press('SIGN IN')
+                ->assertPathIs('/dashboard')
+                ->visit('/company/edit')
+                ->type('name', $faker->company)
+                ->type('crn', $faker->ean8)
+                ->type('domain_name', "invoiceneko.com")
+                ->type('email', $faker->unique()->companyEmail)
+                ->type('phone', '+658' . $faker->numberBetween($min = 1, $max = 8) . $faker->randomNumber(6, true))
+                ->press('UPDATE')
+                ->assertPresent('#edit-company')
+                ->assertInputValue('domain_name', "invoiceneko.com");
         });
     }
 }
