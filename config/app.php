522c812769823c260618b2cacc3080f58a175ca5
Zane J. Chua <zane.jocelyn@gmail.com>
2018-11-07 23:31:01 +0800

    Remove unnecessary provider declaration

--- config/app.php
+++ config/app.php
@@ -170,7 +170,6 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\TelescopeServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
     ],
 
