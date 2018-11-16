74847f206d67da845b7e55ec600fc10b52a10db6
Zane J. Chua <zane.jocelyn@gmail.com>
2018-11-16 09:42:45 +0800

    Fix Design Issue

--- resources/views/pages/forgot.blade.php
+++ resources/views/pages/forgot.blade.php
@@ -8,22 +8,24 @@
 @section("content")
     <div class="wrap v-center">
         <div class="container content-main-authentication">
-            <div class="login-container z-depth-1">
-                <div class="avatar">
-                    <img src="{{ asset('assets/img/avatar.png') }}" width="150" height="150">
-                </div>
-                <hr><br>
-                <div class="form-box">
-                    <form id="forgot" method="post">
-                        <div class="row">
-                            <div class="input-field col s12">
-                                <input id="email" name="email" type="email" data-parsley-required="true" data-parsley-trigger="change">
-                                <label for="email" class="label-validation">Email</label>
+            <div class="login-container">
+                <div class="card-panel">
+                    <div class="avatar">
+                        <img src="{{ asset('assets/img/avatar.png') }}">
+                    </div>
+                    <hr>
+                    <div class="form-box">
+                        <form id="forgot" method="post">
+                            <div class="row">
+                                <div class="input-field col s12">
+                                    <input id="email" name="email" type="email" data-parsley-required="true" data-parsley-trigger="change">
+                                    <label for="email" class="label-validation">Email</label>
+                                </div>
                             </div>
-                        </div>
-                        {{ csrf_field() }}
-                        <button class="btn btn-theme waves-effect waves-light full-width" type="submit" name="action">Send Reset Link</button>
-                    </form>
+                            {{ csrf_field() }}
+                            <button class="btn btn-theme waves-effect waves-light full-width" type="submit" name="action">Send Reset Link</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/pages/reset.blade.php
+++ resources/views/pages/reset.blade.php
@@ -8,39 +8,41 @@
 @section("content")
     <div class="wrap v-center">
         <div class="container content-main-authentication">
-            <div class="login-container z-depth-1">
-                <div id="output"></div>
-                <div class="avatar">
-                    <img src="{{ asset('assets/img/avatar.png') }}" width="150" height="150">
-                </div>
-                <hr><br>
-                <div class="form-box">
-                    <form id="reset" method="post">
-                        <div class="row">
-                            <div class="input-field col s12">
-                                <input id="email" name="email" type="email" data-parsley-required="true" data-parsley-trigger="change">
-                                <label for="email" class="label-validation">Email</label>
-                                <span class="helper-text"></span>
+            <div class="login-container">
+                <div class="card-panel">
+                    <div id="output"></div>
+                    <div class="avatar">
+                        <img src="{{ asset('assets/img/avatar.png') }}">
+                    </div>
+                    <hr>
+                    <div class="form-box">
+                        <form id="reset" method="post">
+                            <div class="row">
+                                <div class="input-field col s12">
+                                    <input id="email" name="email" type="email" data-parsley-required="true" data-parsley-trigger="change">
+                                    <label for="email" class="label-validation">Email</label>
+                                    <span class="helper-text"></span>
+                                </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="input-field col s12">
-                                <input id="password" name="password" type="password" data-parsley-required="true" data-parsley-trigger="change">
-                                <label for="password" class="label-validation">Password</label>
-                                <span class="helper-text"></span>
+                            <div class="row">
+                                <div class="input-field col s12">
+                                    <input id="password" name="password" type="password" data-parsley-required="true" data-parsley-trigger="change">
+                                    <label for="password" class="label-validation">Password</label>
+                                    <span class="helper-text"></span>
+                                </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="input-field col s12">
-                                <input id="password_confirmation" name="password_confirmation" type="password" data-parsley-required="true" data-parsley-trigger="change">
-                                <label for="password" class="label-validation">Password Confirmation</label>
-                                <span class="helper-text"></span>
+                            <div class="row">
+                                <div class="input-field col s12">
+                                    <input id="password_confirmation" name="password_confirmation" type="password" data-parsley-required="true" data-parsley-trigger="change">
+                                    <label for="password" class="label-validation">Password Confirmation</label>
+                                    <span class="helper-text"></span>
+                                </div>
                             </div>
-                        </div>
-                        <input type="hidden" name="token" value="{{ $token }}">
-                        {{ csrf_field() }}
-                        <button class="btn btn-theme waves-effect waves-light full-width" type="submit" name="action">Reset Password</button>
-                    </form>
+                            <input type="hidden" name="token" value="{{ $token }}">
+                            {{ csrf_field() }}
+                            <button class="btn btn-theme waves-effect waves-light full-width" type="submit" name="action">Reset Password</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
