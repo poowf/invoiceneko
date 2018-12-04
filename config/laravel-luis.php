fb2610ef484fb361ccb79f4ee7f2090bf06bd5e9
Zane J. Chua <zane.jocelyn@gmail.com>
2018-12-04 22:36:34 +0800

    Add Composer Package for LUIS

---
+++ config/laravel-luis.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'base_uri' => env('LUIS_BASE_URI'),
+
+    'app_id' => env('LUIS_APP_ID'),
+
+    'subscription_key' => env('LUIS_SUBSCRIPTION_KEY'),
+
+    'staging' => env('LUIS_STAGING'),
+
+    'timezoneOffset' => env('LUIS_TIMEZONE_OFFSET'),
+
+];
