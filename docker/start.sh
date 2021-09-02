80bfab76350a32283de7bb2bc11923a366e2cfe2
Zane J. Chua <zane@poowf.com>
2021-09-03 02:30:18 +0800

    ci: add working docker configuration

--- docker/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rm -rf /etc/nginx/sites-enabled/default
-ln -s /etc/nginx/sites-available/invoiceneko-nginx.conf /etc/nginx/sites-enabled/invoiceneko-nginx.conf
-/usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
