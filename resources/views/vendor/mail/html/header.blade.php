96c8f313eb6cbe0f5a90bd38ae5840109fb592c5
Zane J. Chua <zane@poowf.com>
2021-09-04 03:48:59 +0800

    fix: branding missing from email

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -4,6 +4,7 @@
 @if (trim($slot) === 'Laravel')
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
+<div style="padding-bottom: 10px;"><img src="{{ asset('assets/img/logo.png') }}" height="70" alt="InvoiceNeko"/></div>
 {{ $slot }}
 @endif
 </a>
--- resources/views/vendor/mail/text/message.blade.php
+++ resources/views/vendor/mail/text/message.blade.php
@@ -22,6 +22,8 @@
     @slot('footer')
         @component('mail::footer')
             © {{ date('Y') }} {{ config('app.name') }}. @lang('All rights reserved.')
+            <a href="{{ route('privacy') }}" target="_blank" rel="nofollow noopener noreferrer">Privacy Policy</a>
+            <a href="{{ route('terms') }}" target="_blank" rel="nofollow noopener noreferrer">Terms & Conditions</a>
         @endcomponent
     @endslot
 @endcomponent
